3508a4ddf5183112868b048e27492fe99b06250e
Victor Hugo Vazquez Gomez <hugovg799@gmail.com>
2023-11-19 11:37:28 -0600

    remove prettier and add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,6 @@ module.exports = {
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
   ],
   root: true,
   env: {
@@ -21,5 +20,60 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    "indent": [
+      "error",
+      2,
+      {
+        "SwitchCase": 1,
+        "ignoredNodes": ["PropertyDefinition"]
+      }
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "no-trailing-spaces": [
+      "error",
+      { "ignoreComments": true }
+    ],
+    "max-statements-per-line": [
+      "error",
+      { "max": 1 }
+    ],
+    "quotes": [
+      "error",
+      "double"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ],
+    "no-useless-catch": [
+      "off",
+      "always"
+    ],
+    "eqeqeq": [
+      "error",
+      "smart"
+    ],
+    "no-var": [
+      "error",
+    ],
+    "no-multiple-empty-lines": [
+      "error",
+      {
+        "max": 1,
+        "maxEOF": 1,
+        "maxBOF": 0
+      }
+    ],
+    "padded-blocks": [
+      "error",
+      "never"
+    ],
+    "one-var": [
+      "error",
+      "never"
+    ],
   },
 };
